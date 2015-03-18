1ea3892cf7330a551912a3f6b39462a8d869ea35
Ken Wilcox <wilcoxk@gmail.com>
2015-03-17 22:41:16 -0600

    Added a view for a button

---
+++ WatchKitUI WatchKit Extension/ButtonInterfaceController.swift
@@ -0,0 +1,36 @@
+//
+//  ButtonInterfaceController.swift
+//  WatchKitUI
+//
+//  Created by Kenneth Wilcox on 3/17/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ButtonInterfaceController: WKInterfaceController {
+  
+  @IBOutlet weak var myButton: WKInterfaceButton!
+  
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    // Configure interface objects here.
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  @IBAction func buttonPressed() {
+    
+  }
+}
